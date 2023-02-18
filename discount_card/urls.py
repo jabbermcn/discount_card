445f99fdc4d4a9d4e930080f8554f1bd32404533
Mikalai Mikhailouski <jabber_mcn@tut.by>
2023-02-18 14:23:09 +0300

    api feature

--- discount_card/urls.py
+++ discount_card/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('discount.urls'))
+    path('', include('discount.urls')),
+    path('api/', include('api.urls')),
 ]
